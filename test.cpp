5e22a94f0afccdb922d3e975076cdf94aacda564
Jarfi_Laptop <jarvisang@stu.xidian.edu.cn>
2022-03-22 17:46:34 +0800

    upload in laptop

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout << "这是一个测试的cpp文件" << endl;
+	cout << "测试在laptop上" << endl;
+	cout << "测试内容是：window上又创建了一个密钥对来连接github" << endl;
+	cout << "这里是查看是否可以直接上传（经过密钥对）" << endl;
+
+	return 0;
+}
